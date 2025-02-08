a84570a894c3e6a27e6e532bb5df7406af4ad59c
Mishka56 <mishka_19@icloud.com>
2025-02-08 21:50:12 +0100

    update 2.0 task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,7 @@
 function makeArray(firstArray, secondArray, maxLength) {
-const result = firstArray.concat(secondArray);
-	if(result.length >= maxLength) {
-return result.slice(0, maxLength);
-	} else {
-		return result;
-	}
-
-}
+return firstArray.concat(secondArray).slice(0, maxLength);
+	} 
+	
 
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
